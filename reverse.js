24576fb87ee250cf2a17b05aa488affaf2d6372b
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-07-28 11:37:02 +0600

    js basic problem solving for reverse

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function ReverseIN () {
+     const Alomgr = "hello";
+    const reverse = Alomgr.split("").reverse().join("");
+    console.log(reverse)
+    
+}
+ReverseIN()
+
+export default ReverseIN;
