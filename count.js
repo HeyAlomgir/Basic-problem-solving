8acc99ae9f8bd2f98062311e443ba8bc9cd825af
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-07-29 03:26:03 +0600

    coutn aded

---
+++ count.js
@@ -0,0 +1,13 @@
+function countVowel(str){
+     let count = 0;
+    const vowels ="aeiouAEIOU";
+
+    for (let char of str){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+    return count;
+
+}
+console.log(countVowel("programming"));
